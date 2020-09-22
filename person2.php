580a90c2de1c2d69ab75fd8e26186f6da458a93c
FlorianB04 <florianb04@gmail.com>
2020-09-22 12:16:10 +0200

    create person2.php to see 'static' state

---
+++ person2.php
@@ -0,0 +1,45 @@
+<?php 
+
+class Person // creation de la classe (bluePrint)
+    {
+        public $firstName='eric';
+        public $lastName;
+        public $age;
+
+
+        protected static $totalCount = 0 ;
+
+
+        public function __construct()
+        {
+            static::$totalCount++;
+        }
+
+
+        // getter = donne la possibilité d'afficher les variables qui seront en private
+    public static function getTotalCount ()
+        {
+            return static::$totalCount;
+        }
+
+    }   
+
+$person1 = new Person;
+$person2 = new Person;
+$person3 = new Person;
+
+// façon d'acceder avec le :: aux propriétes static de la class
+echo Person::getTotalCount();
+
+
+
+
+
+
+// echo '<pre>' ;
+// print_r($prof1);
+// echo '</pre>' ;
+
+
+
+?>
